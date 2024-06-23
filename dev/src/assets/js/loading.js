aeebea2ea70314266b34c49745a88a7fb5ee1397
pumiWebdesigner <pumi.webdesigner@gmail.com>
2024-06-23 12:42:12 +0900

    loadingが終わったらの条件追加

--- dev/src/assets/js/loading.js
+++ dev/src/assets/js/loading.js
@@ -1,10 +1,12 @@
 {
-  // 1秒待ってからloadediengのクラスを付与する
-  setTimeout(function () {
-    jQuery(".p-loading__bg").addClass("loaded");
-  }, 1000);
-  // 2秒待ってからdisplay:none;する
-  setTimeout(function () {
-    jQuery(".p-loading__bg").css("display", "none");
-  }, 2000);
+  window.onload = function () {
+    // 1秒待ってからloadediengのクラスを付与する
+    setTimeout(function () {
+      jQuery(".p-loading__bg").addClass("loaded");
+    }, 1000);
+    // 2秒待ってからdisplay:none;する
+    setTimeout(function () {
+      jQuery(".p-loading__bg").css("display", "none");
+    }, 2000);
+  };
 }
